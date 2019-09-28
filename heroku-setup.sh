fe4b6cbde0b12e5b8dc027381a5efec55f921362
Harikrushna V. Adiecha <adiechahari@gmail.com>
2019-09-28 22:06:24 +0530

    setup auth

---
+++ heroku-setup.sh
@@ -0,0 +1,6 @@
+heroku config:set DB_CONNECTION=pgsql --app expence
+heroku config:set DB_HOST=ec2-79-125-2-142.eu-west-1.compute.amazonaws.com --app expence
+heroku config:set DB_PORT=5432 --app expence
+heroku config:set DB_DATABASE=du6h2ui7cq3nh --app expence
+heroku config:set DB_USERNAME=wthdafpaqgempa --app expence
+heroku config:set DB_PASSWORD=4225efa286107c2ef123b2fe170cb38d2da26db3234e57ac287d040ca59a1ed4 --app expence
